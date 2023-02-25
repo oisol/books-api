22cee677141269711912de6263fe0412f6a3547a
Matheus <matheusroca11@gmail.com>
2023-02-24 22:42:24 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World')
+});
+
+server.listen(PORT), () => {
+    console.log(`Servidor Rodando na Porta ${PORT}`)
+};
